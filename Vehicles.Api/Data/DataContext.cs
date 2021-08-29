3edf6d0999130a05cdf37587e74789d26a28af87
Usuario <Usuario@JOSEMIGUEL155>
2021-08-29 13:42:57 -0500

    se creo los tipos de vehiculos

---
+++ Vehicles.Api/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.Api.Data.Entities;
+
+namespace Vehicles.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        //constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        //nombre de la tabla a crear (clase )
+        public DbSet<VehiclesType> VehiclesType { get; set; }
+        //se sobre escribe el metodo OnModelCreating 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehiclesType>().HasIndex(x => x.Description).IsUnique(); //se especifica que el campo va a ser unico
+        }
+        
+    }
+}
---
+++ Vehicles.Api/Data/Entities/VehiclesType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.Api.Data.Entities
+{
+    public class VehiclesType
+    {//propieddes de la clase
+        public int Id { get; set; }
+
+        [Display(Name="Tipo de Vehículo")]//nombre que va a ver el usuario
+        [MaxLength(50,ErrorMessage ="El campo {0} no puede tener mas de {1} Caracteres")]//maximo del campo y error en caso de sobrepasar el tamaño
+        [Required (ErrorMessage="El campo {0} es Obligatorio")] //DataAnnotations para hacer el campo requerido(ErrorMessage = mensaje personalizado)
+        public string Description { get; set; }
+    }
+}
